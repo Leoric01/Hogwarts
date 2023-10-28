85347d69480d6ce7e7795e478db57c9d318cb7a2
Leoric01 <troglodit.nick@seznam.cz>
2023-10-28 12:30:36 +0200

    added default data for users

--- Hogwarts/src/main/java/com/leoric01/hogwarts/system/DBDataInitializer.java
+++ Hogwarts/src/main/java/com/leoric01/hogwarts/system/DBDataInitializer.java
@@ -1,8 +1,10 @@
 package com.leoric01.hogwarts.system;
 
 import com.leoric01.hogwarts.models.artifact.Artifact;
+import com.leoric01.hogwarts.models.hogwartsuser.HogwartsUser;
 import com.leoric01.hogwarts.models.wizard.Wizard;
 import com.leoric01.hogwarts.respositories.ArtifactRepository;
+import com.leoric01.hogwarts.respositories.UserRepository;
 import com.leoric01.hogwarts.respositories.WizardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,10 +16,12 @@ public class DBDataInitializer implements CommandLineRunner {
     private final ArtifactRepository artifactRepository;
 
     private final WizardRepository wizardRepository;
+    private final UserRepository userRepository;
     @Autowired
-    public DBDataInitializer(ArtifactRepository artifactRepository, WizardRepository wizardRepository) {
+    public DBDataInitializer(ArtifactRepository artifactRepository, WizardRepository wizardRepository, UserRepository userRepository) {
         this.artifactRepository = artifactRepository;
         this.wizardRepository = wizardRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
@@ -49,6 +53,30 @@ public class DBDataInitializer implements CommandLineRunner {
         artifactRepository.save(a4);
         artifactRepository.save(a5);
         artifactRepository.save(a6);
+
+        HogwartsUser u1 = new HogwartsUser();
+        u1.setId(1L);
+        u1.setUsername("john");
+        u1.setPassword("123");
+        u1.setEnabled(true);
+        u1.setRoles("ADMIN USER");
+
+        HogwartsUser u2 = new HogwartsUser();
+        u2.setId(2L);
+        u2.setUsername("eric");
+        u2.setPassword("321");
+        u2.setEnabled(false);
+        u2.setRoles("USER");
+
+        HogwartsUser u3 = new HogwartsUser();
+        u3.setId(3L);
+        u3.setUsername("monika");
+        u3.setPassword("ccc");
+        u3.setEnabled(true);
+        u3.setRoles("ADMIN");
+        userRepository.save(u1);
+        userRepository.save(u2);
+        userRepository.save(u3);
 
     }
 }
